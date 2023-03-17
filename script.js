2e1ea8ab8e0c1eb4c048e8c089a788af8b6b125c
Rilnga <nuelagyei992@gmial.com>
2023-03-17 11:46:29 +0000

    Javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+let js ='amazing';
+if (js === 'amazing') alert('Javascript is fun!');
+console.log(40 + 8 + 23 - 10);
+
+console.log("Nuel");
+
+let firstName = "Melody";
+console.log(firstName);
+
+let LOVE = "GOD";
+console.log("LOVE")
+
+let country = 'Ghana';
+console.log(country);
+
+let continent = 'Africa';
+console.log(continent);
+
+let population = '1000';
+console.log(population);
