30e7a34c12dcbc6ec6bbb4b98b788b7185e3d7ae
Rilnga <nuelagyei992@gmial.com>
2023-03-17 15:11:07 +0000

    Additions

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let js ='amazing';
-if (js === 'amazing') alert('Javascript is fun!');
-console.log(40 + 8 + 23 - 10);
+// let js ='amazing';
+// if (js === 'amazing') alert('Javascript is fun!');
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Nuel");
+// console.log("Nuel");
 
-let firstName = "Melody";
-console.log(firstName);
+// let firstName = "Melody";
+// console.log(firstName);
 
-let LOVE = "GOD";
-console.log("LOVE")
+// let LOVE = "GOD";
+// console.log("LOVE")
 
-let country = 'Ghana';
-console.log(country);
+// let country = 'Ghana';
+// console.log(country);
 
-let continent = 'Africa';
-console.log(continent);
+// let continent = 'Africa';
+// console.log(continent);
 
-let population = 'One thousand';
-console.log(population);
+// let population = 'One thousand';
+// console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof 'Melody')
+
+javascriptIsFun = 'YES!';  
