85540bf4c280094f79a5879033047fd460e29e80
Rilnga <nuelagyei992@gmial.com>
2023-03-27 09:31:52 +0000

    equality operator

--- script.js
+++ script.js
@@ -32,83 +32,92 @@
 // job = 'Weed Student'; 
 
 
-const ageNuel = 2026 - 2003;
-const ageMelody = 2026 - 2004;
-console.log (ageNuel, ageMelody);
+// const ageNuel = 2026 - 2003;
+// const ageMelody = 2026 - 2004;
+// console.log (ageNuel, ageMelody);
 
-console.log(ageNuel * 2, ageMelody/2);
+// console.log(ageNuel * 2, ageMelody/2);
 
-let x = 10 + 5;
-x += 10;
-x *= 4;
-console.log(x);
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// console.log(x);
 
-//comparason operator
-console.log(ageNuel > ageMelody);
-console.log(ageMelody >= 18);
+// //comparason operator
+// console.log(ageNuel > ageMelody);
+// console.log(ageMelody >= 18);
 
-//challenge
+// //challenge
 
-const heightNuel =1.72;
-const massNuel =56;
+// const heightNuel =1.72;
+// const massNuel =56;
 
-const heightMel = 1.62;
-const massMel = 52;
+// const heightMel = 1.62;
+// const massMel = 52;
 
-let bmiNuel = massNuel/heightNuel;
-let bmiMel = massMel/heightMel;
+// let bmiNuel = massNuel/heightNuel;
+// let bmiMel = massMel/heightMel;
 
-console.log(bmiNuel);
+// console.log(bmiNuel);
 
-console.log(bmiMel);
+// console.log(bmiMel);
 
 
-console.log(bmiMel > bmiNuel);
+// console.log(bmiMel > bmiNuel);
 
-if(bmiMel > bmiNuel){
-    console.log("She's bigger than me")
-} else{
-    console.log("It's a lie!")
+// if(bmiMel > bmiNuel){
+//     console.log("She's bigger than me")
+// } else{
+//     console.log("It's a lie!")
+// }
+
+
+
+
+// //Strings and template
+
+
+// const secondName = 'Nuel';
+// const work = 'Student';
+// const dob = 2003;
+// const year = 2023;
+
+// const nuel = "I'm"+ secondName 
+
+// const nuelNew = `I'm ${secondName} `
+
+// //taking decisions
+
+// const age = 16;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough) {
+//  console.log('Approved')
+// } else {
+//  console.log('Unsatisfied minimum requirements')
+// }
+
+
+// //Type conversion 
+
+// const inputYear = '2003 ';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18)
+
+
+// //Type coercion
+//  console.log('I am' + 23 + 'years old');
+//  console.log('23' - '10' - 3);
+//  console.log('23'/'2')
+//  console.log('23'>'18');
+
+
+//  //Truthy and falsy
+//  console.log(Boolean(0))
+
+//Equality operator
+
+const age = 18;
+if(age === 18){
+    console.log("You're an adult")
 }
-
-
-
-
-//Strings and template
-
-
-const secondName = 'Nuel';
-const work = 'Student';
-const dob = 2003;
-const year = 2023;
-
-const nuel = "I'm"+ secondName 
-
-const nuelNew = `I'm ${secondName} `
-
-//taking decisions
-
-const age = 16;
-const isOldEnough = age >= 18;
-
-if(isOldEnough) {
- console.log('Approved')
-} else {
- console.log('Unsatisfied minimum requirements')
-}
-
-
-//Type conversion 
-
-const inputYear = '2003 ';
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18)
-
-
-//Type coercion
- console.log('I am' + 23 + 'years old');
- console.log('23' - '10' - 3);
- console.log('23'/'2')
- console.log('23'>'18');
-
-
